1e9a8547e7f57f89832ba41467345fab3b54da12
P4n0pt1c0n <davidbsanchez813@gmail.com>
2025-11-18 22:35:39 +0000

    Program that generates a possible band name for a user.

---
+++ bandname-generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator program.")
+
+city_name = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your first pet?\n")
+print("The name of your band could be " + city_name + " " + pet_name + ".")
