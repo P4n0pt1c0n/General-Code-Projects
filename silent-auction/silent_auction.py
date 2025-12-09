2ccd6fb021495cd51254e317a0126a2874db644d
P4n0pt1c0n <davidbsanchez813@gmail.com>
2025-12-09 20:06:29 +0000

    Program for silent auction bidding.

---
+++ silent-auction/silent_auction.py
@@ -0,0 +1,27 @@
+from art import logo
+
+def find_highest_bidder (bidding_dictionary):
+    max_bid = 0
+    max_name = ""
+    for name in bidding_dictionary:
+        if bidding_dictionary[name] > max_bid:
+            max_name = name
+            max_bid = bid_dict[name]
+    print(f"The winner of the auction is {max_name} with a bid of ${max_bid}")
+
+bid_dict = {}
+continue_bid = True
+
+print(logo)
+print("Welcome to the secret auction program.")
+
+while continue_bid:
+    username = input("What is your name?: ")
+    bid = int(input("What's your bid?: $"))
+    bid_dict.update({username: bid})
+    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'. \n").lower()
+    if other_bidders == 'no':
+        continue_bid = False
+        find_highest_bidder(bid_dict)
+    elif other_bidders == 'yes':
+        print("\n" * 20)
