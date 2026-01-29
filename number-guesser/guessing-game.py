57a473eacf706fcf15eb4301119994de6ae19367
P4n0pt1c0n <davidbsanchez813@gmail.com>
2026-01-29 21:16:58 +0000

    Adjust turn limits and improve guess prompts

--- number-guesser/guessing-game.py
+++ number-guesser/guessing-game.py
@@ -1,15 +1,15 @@
 import random
 from art import logo
 
-EASY_TURNS = 11
-HARD_TURNS = 6
+EASY_TURNS = 10
+HARD_TURNS = 5
 
 def low_or_high(user_num, num_guess):
     if user_num < num_guess:
-        print("Too low.\nGuess again.")
+        print("Too low.")
         return False
     elif user_num > num_guess:
-        print("Too high.\nGuess again.")
+        print("Too high.")
         return False
     elif user_num - num_guess == 0:
         print(f"You got it! The answer was {num_guess}")
@@ -22,18 +22,27 @@ def set_difficulty(difficulty):
         return HARD_TURNS
 
 def easy_guess(rand_num):
-    for i in range(1, 11):
-        print(f"You have {11 - i} attempts remaining to guess the number.")
+    for i in range(EASY_TURNS):
+        print(f"You have {EASY_TURNS - i} attempts remaining to guess the number.")
         user_guess = int(input("Make a guess: "))
         if low_or_high(user_num=user_guess, num_guess=rand_num):
             return
+        elif i == 9:
+            print("You've run out of guesses, you lose.")
+            return
+        print("Guess again.")
 
 def hard_guess(rand_num):
-    for i in range(1, 6):
-        print(f"You have {6 - i} attempts remaining to guess the number.")
+    for i in range(HARD_TURNS):
+        print(f"You have {HARD_TURNS - i} attempts remaining to guess the number.")
         user_guess = int(input("Make a guess: "))
         if low_or_high(user_num=user_guess, num_guess=rand_num):
             return
+        elif i == 4:
+            print("You've run out of guesses, you lose.")
+            return
+        print("Guess again.")
+    print()
 
 def number_guesser():
     print(logo)
@@ -42,10 +51,6 @@ def number_guesser():
     print("I'm thinking of a number between 1 and 100.")
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
-    turn_num = set_difficulty(difficulty)
-    for i in range(1, turn_num):
-        print(f"You have {turn_num - i} attempts remaining to guess the number.")
-        user_guess = int(input("Make a guess: "))
     if difficulty == 'easy':
         easy_guess(rand_num)
     elif difficulty == 'hard':
