1e4e58979728dbd614bcddbee138d4245ab64869
P4n0pt1c0n <davidbsanchez813@gmail.com>
2026-02-02 21:09:52 +0000

    Refactor guessing game logic for clarity and efficiency
    Commented code to provide additional clarity on program's functions and designs.

--- number-guesser/guessing-game.py
+++ number-guesser/guessing-game.py
@@ -5,6 +5,7 @@ EASY_TURNS = 10
 HARD_TURNS = 5
 
 def low_or_high(user_num, num_guess):
+    # Returns False if guessed num. is incorrect to resume loop.
     if user_num < num_guess:
         print("Too low.")
         return False
@@ -15,45 +16,37 @@ def low_or_high(user_num, num_guess):
         print(f"You got it! The answer was {num_guess}")
         return True
 
-def set_difficulty(difficulty):
-    if difficulty == 'easy':
+def set_difficulty(turn_choice):
+    # Return num. of turns to 10 if easy, 5 if hard.
+    if turn_choice == 'easy':
         return EASY_TURNS
-    elif difficulty == 'hard':
+    elif turn_choice == 'hard':
         return HARD_TURNS
 
-def easy_guess(rand_num):
-    for i in range(EASY_TURNS):
-        print(f"You have {EASY_TURNS - i} attempts remaining to guess the number.")
+def guess_loop(turn_count, num_to_guess):
+    i = 0
+    while i <= turn_count - 1:
+        print(f"You have {turn_count - i} attempts remaining to guess the number.")
         user_guess = int(input("Make a guess: "))
-        if low_or_high(user_num=user_guess, num_guess=rand_num):
+        # If user guesses correct num., return True and end loop early.
+        if low_or_high(user_num=user_guess, num_guess=num_to_guess):
             return
-        elif i == 9:
+        # If user runs out of guesses, print lose message then exit function early.
+        elif i + 1 == turn_count:
             print("You've run out of guesses, you lose.")
             return
         print("Guess again.")
-
-def hard_guess(rand_num):
-    for i in range(HARD_TURNS):
-        print(f"You have {HARD_TURNS - i} attempts remaining to guess the number.")
-        user_guess = int(input("Make a guess: "))
-        if low_or_high(user_num=user_guess, num_guess=rand_num):
-            return
-        elif i == 4:
-            print("You've run out of guesses, you lose.")
-            return
-        print("Guess again.")
-    print()
+        i += 1
 
 def number_guesser():
     print(logo)
-    rand_num = random.randrange(1, 101)
+    rand_num = random.randrange(1, 101) # Generate rand. num. to guess.
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-
-    if difficulty == 'easy':
-        easy_guess(rand_num)
-    elif difficulty == 'hard':
-        hard_guess(rand_num)
+    # Sets number of turns based on difficulty chosen, which will be passed to guessing function.
+    turns = set_difficulty(turn_choice=difficulty)
+    # Starts guess loop function. Pass num. of turns and num. to guess into function.
+    guess_loop(turn_count=turns, num_to_guess=rand_num)
 
 number_guesser()
